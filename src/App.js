7ec6f99b3f11c35525e564d073a3807c013bf18e
Dorje <rtorchiiw@gmail.com>
2019-12-10 10:57:34 +0545

    render Game container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Board from './components/Board';
+import Game from './containers/Game';
 
 function App() {
   return (
     <div className="App">
-      <Board />
+      <Game />
     </div>
   );
 }
