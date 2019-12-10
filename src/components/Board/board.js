07daefc6de1fc0a8ce5104f1582d4ca36cf45915
Dorje <rtorchiiw@gmail.com>
2019-12-10 10:59:38 +0545

    Dynamic box number

--- src/components/Board/board.js
+++ src/components/Board/board.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react';
-
-
+import React, { Component, Fragment } from 'react';
+import  './board.scss';
 export default class Board extends Component {
   constructor(props){
     super(props)
     this.state = {
-
+      perRow: 4
     }
   }
 
-  componentDidMount(){
-
-  }
-
   render(){
-    return <div> This is board </div>
+    const { perRow } = this.state;
+    const Rows = [];
+    const Columns = []
+    for( let i = 0; i < perRow; i++){
+      Rows.push(<div className="p-2 square"></div>)
+    }
+
+    for (let index = 0; index < perRow; index++) {
+      Columns.push(<div className="d-flex d-row"> { Rows } </div>)
+    }
+    
+    return(
+      <Fragment>
+        <div className="d-flex flex-column">
+          {Columns}
+        </div>
+      </Fragment>
+    )
   }
 }
