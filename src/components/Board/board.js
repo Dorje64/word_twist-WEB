e23e77a2d46c9c73e78f961d1b1578d3bb4d3644
Dorje <rtorchiiw@gmail.com>
2019-12-11 10:59:08 +0545

    get props and place alphabates in boxes

--- src/components/Board/board.js
+++ src/components/Board/board.js
@@ -1,23 +1,42 @@
 import React, { Component, Fragment } from 'react';
 import  './board.scss';
+
+const alphabates = 'abcdefghijklmnopqrstuvwxyz'.split('');
 export default class Board extends Component {
   constructor(props){
     super(props)
     this.state = {
-      perRow: 4
+      perRow: this.props.perRow || 4,
+      borderColor: this.props.borderColor || 'coral',
+      borderWidth: this.props.borderWith || '4px', 
     }
+  }
+
+  generateRow = (col) => {
+    const { perRow } = this.state; 
+    const {borderColor, borderWidth} = this.state;
+    const Rows = [];
+
+    for( let i = 0; i < perRow; i++){
+      const letter = alphabates.random();
+      // this.setState({table[col][i]:letter })
+      Rows.push(<div className="square" 
+                style={{borderColor: borderColor, borderWidth: borderWidth }} 
+                key={`${col}-${i}`}
+                onClick={_ => { this.props.getLetter(letter) }}
+                >
+        <div className="content">  {letter} </div> 
+      </div>)
+    }
+    return Rows;
   }
 
   render(){
     const { perRow } = this.state;
-    const Rows = [];
     const Columns = []
-    for( let i = 0; i < perRow; i++){
-      Rows.push(<div className="p-2 square"></div>)
-    }
 
     for (let index = 0; index < perRow; index++) {
-      Columns.push(<div className="d-flex d-row"> { Rows } </div>)
+      Columns.push(<div className="d-flex d-row" key={index}> { this.generateRow(index)} </div>)
     }
     
     return(
@@ -28,4 +47,8 @@ export default class Board extends Component {
       </Fragment>
     )
   }
+}
+
+Array.prototype.random = function () {
+  return this[Math.floor((Math.random()*this.length))];
 }
