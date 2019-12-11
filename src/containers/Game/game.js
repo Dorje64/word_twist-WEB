8b9428e668465463d2b5e52f9081ecda2bbdca18
Dorje <rtorchiiw@gmail.com>
2019-12-11 11:04:34 +0545

    send props to board

--- src/containers/Game/game.js
+++ src/containers/Game/game.js
@@ -7,18 +7,28 @@ export default class Game extends Component {
   constructor(props){
     super(props)
     this.state = {
-
+      word: ''
     }
   }
 
   componentDidMount(){
 
   }
+  getLetter = letter => {
+    console.log(letter)
+  }
 
   render(){
     return <div className="row">
-      <div className="col bg-info full-height">
-        <Board />
+      <div className="col bg-info full-height  pt-5">
+        <div className="board-wrapper mx-auto">
+        <Board 
+          perRow = {4}
+          borderColor = 'abc'
+          borderWidth = '2px'
+          getLetter = {this.getLetter}
+        />
+        </div>
       </div>
       <div className="col bg-success full-height">
           <Stats />
@@ -27,3 +37,4 @@ export default class Game extends Component {
   }
 
 }
+
