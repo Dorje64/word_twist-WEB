c97db0c56721910096ea3417910010c38f5a12fe
Dorje <rtorchiiw@gmail.com>
2019-12-13 19:06:42 +0545

    add http on url

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,7 +1,12 @@
 import Axios from 'axios';
 
-const API = Axios.create({ baseURL: 'localhost:3000' })
+const API = Axios.create({ baseURL: 'http://relocalhost:3000/' })
 
 export const ValidWord = (word) => {
-  return API.get(`/word/check?word=${word}`)
+
+  return API.get(`?word=${word}`,{
+    headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json'
+	}})
 }
