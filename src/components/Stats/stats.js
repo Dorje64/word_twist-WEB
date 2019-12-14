ace1adbf2e3d10c2f3f929367384757b28ae1f78
Dorje <rtorchiiw@gmail.com>
2019-12-14 22:50:23 +0545

    add stats components

--- src/components/Stats/stats.js
+++ src/components/Stats/stats.js
@@ -1,17 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import './style.scss';
+
 export default class Stats extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
+  // constructor(props){
+  //   super(props)
+  //   this.state = {
+  //     correctsWords: this.props.correctsWords,
+  //     inCorrentWords: this.props.inCorrentWords
+  //   }
+  // }
 
-    }
-  }
+  // componentDidMount(){
 
-  componentDidMount(){
-
-  }
+  // }
 
   render(){
-    return <div> This is STats </div>
+    const { correctsWords, inCorrentWords } = this.props; 
+    return (
+      <Fragment>
+         
+            <div className="list-group d-flex flex-column-reverse word-stack">
+            { correctsWords.map(word => Word(word, 'success')) }
+            <h4 className="text-light"> Corrects Stack</h4>
+            </div>
+              
+            <div className= "list-group d-flex flex-column-reverse word-stack">
+              { inCorrentWords.map(word => Word(word, 'danger')) }
+              <h4 className="text-light">Incorrect Stack</h4>
+            </div>
+         
+      </Fragment>
+    )
   }
 }
+
+const Word = (word,style) => <li className={"list-group-item list-group-item-" + style} key={word}> 
+                          {word}
+                          <span className={`badge badge-${style} badge-pill float-right`}>{word.length}</span>
+                        </li>
+                       
